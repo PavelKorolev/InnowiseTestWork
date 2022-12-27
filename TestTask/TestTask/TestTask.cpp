7106cd0d7358e00cabab228cc578d260785f8cc5
PavelKorolev <koroljovpi03@gmail.com>
2022-12-27 21:05:06 +0300

    TestTask

---
+++ TestTask/TestTask/TestTask.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <unordered_map>
+using namespace std;
+const int n = 500;
+
+void findPair(int nums[], int n, int target)
+{
+    // создаем пустую карту
+    unordered_map<int, int> map;
+
+    // делаем для каждого элемента
+    for (int i = 0; i < n; i++)
+    {
+        // проверяем, существует ли пара (nums[i], target - nums[i])
+
+        // если разница была видна раньше, выводим пару
+        if (map.find(target - nums[i]) != map.end())
+        {
+            cout << nums[map[target - nums[i]]] << endl << nums[i] << "\n";
+            return;
+        }
+
+        // сохраняем индекс текущего элемента на карте
+        map[nums[i]] = i;
+    }
+
+    // доходим сюда, если пара не найдена
+    cout << "Pair not found";
+}
+
+int main()
+{
+    int nums[n];
+    fstream f;
+    f.open("Input task1.txt");
+    if (f) {
+        for (int i = 0; i < n; i++)
+        {
+            f >> nums[i];
+        }
+    }
+    
+    int target = 2023;
+    findPair(nums, n, target);
+    f.close();
+
+    return 0;
+}
---
+++ TestTask/TestTask№2/TestTask№2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a[] = { 1, 1, 3, 7, 9 };
+	int b[] = { 1, 4, 12, 12, 76 };
+	
+	int counta = sizeof(a) / sizeof(a[0]);
+	int countb = sizeof(b) / sizeof(b[0]);
+
+	int countc = counta + countb;
+	
+	int* c = new int[countc];
+
+	
+	for (int k = 0; k < countc; k++)
+	{
+		if (k < counta) { c[k] = a[k]; }
+		else { c[k] = b[k - counta]; }
+	}
+
+	sort(c, c + countc);
+
+	cout << "sort array" << endl;
+	for (int i = 0; i < countc; i++)
+	{
+		cout << c[i] << " ";
+	}
+	cout << endl;
+}
